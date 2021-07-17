419b256ae8966db760e84e683dfe15c882a876cf
sourav <souravsouravnag@gmail.com>
2021-07-17 22:01:55 +0530

    Bug Fix

--- advlibrary/src/main/java/com/bkt/advlibrary/PagerAdapter.kt
+++ advlibrary/src/main/java/com/bkt/advlibrary/PagerAdapter.kt
@@ -1,13 +1,10 @@
 package com.bkt.advlibrary
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.bkt.advlibrary.AdvFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.android.synthetic.main.fragment_stockist_transaction.*
 
 class PagerAdapter(
     private val parent: AdvFragment
