67335e57d4d2c6ed1f3fbba061e244a09c63fe63
sourav <souravsouravnag@gmail.com>
2024-05-19 14:02:48 +0530

    Changed: Changed to new class

--- advlibrary/src/main/java/com/bkt/advlibrary/bind/ComplexBindAdapter.kt
+++ advlibrary/src/main/java/com/bkt/advlibrary/bind/ComplexBindAdapter.kt
@@ -2,113 +2,93 @@ package com.bkt.advlibrary.bind
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bkt.advlibrary.bgBlock
-import com.bkt.advlibrary.mainLaunch
 
-class ComplexBindAdapter<Value : Any, B : ViewDataBinding>(
-    @LayoutRes private val layoutId: Int,
-    itemEquals: (item1: Value, item2: Value) -> Equality = { _, _ -> Equality.ITEMS_ARE_DIFFERENT },
-    private val onBind: (b: B, item: Value, position: Int) -> Unit
-) : ListAdapter<Value, ComplexBinderHolder>(object :
+
+/**
+ * Param: [layoutConfig] -> Input is Position. Output should be LayoutId
+ */
+class ComplexBindAdapter<Value>(
+    private val areItemsTheSame: (Value, Value) -> Boolean = { v1, v2 -> v1 == v2 },
+    private val areContentsTheSame: (Value, Value) -> Boolean = { v1, v2 -> v1 == v2 },
+    private val layoutConfig: (Int, Value) -> Int
+) : ListAdapter<Value, ComplexBinderHolder<ViewDataBinding>>(object :
     DiffUtil.ItemCallback<Value>() {
-    override fun areItemsTheSame(oldItem: Value, newItem: Value): Boolean {
-        return itemEquals.invoke(oldItem, newItem).value
+
+    override fun areItemsTheSame(oldItem: Value & Any, newItem: Value & Any): Boolean {
+        return areItemsTheSame.invoke(oldItem, newItem)
     }
 
-    override fun areContentsTheSame(oldItem: Value, newItem: Value): Boolean {
-        return itemEquals.invoke(oldItem, newItem).value
+    override fun areContentsTheSame(oldItem: Value & Any, newItem: Value & Any): Boolean {
+        return areContentsTheSame.invoke(oldItem, newItem)
     }
-}) {
+}), Filterable {
+    // mapping between layout id and binding
+    private val layoutToBindMap =
+        HashMap<Int, (b: ViewDataBinding, item: Value, position: Int) -> Unit>()
+
     private var dataList = ArrayList<Value>()
-    private var submittedList = ArrayList<Value?>()
-    private var filterCondition = { _: Value, _: String? -> true }
-    private val extraPositionList = ArrayList<Int>()
-    private val extraPositionsLayoutMap = LinkedHashMap<Int, ExtraLayouts>()
+    var filterCondition = { _: Value, _: String -> true }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComplexBinderHolder {
+    override fun getItemViewType(position: Int): Int {
+        return layoutConfig.invoke(position, getItem(position))
+    }
+
+    fun <B : ViewDataBinding> addLayoutBind(
+        @LayoutRes layoutId: Int,
+        onBind: (b: B, item: Value, position: Int) -> Unit
+    ) {
+        layoutToBindMap[layoutId] = onBind as (ViewDataBinding, Value, Int) -> Unit
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ComplexBinderHolder<ViewDataBinding> {
         val inflater = LayoutInflater.from(parent.context)
-        val extraLayout = extraPositionsLayoutMap[viewType] // using viewType as position
-        val binding: ViewDataBinding =
-            if (extraLayout != null)
-                DataBindingUtil.inflate(inflater, extraLayout.layoutId, parent, false)
-            else
-                DataBindingUtil.inflate(inflater, layoutId, parent, false)
+        // viewType is the layoutId in our case
+        val binding: ViewDataBinding = DataBindingUtil.inflate(inflater, viewType, parent, false)
         return ComplexBinderHolder(binding)
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return position // using position as viewType
-    }
-
-    override fun onBindViewHolder(holder: ComplexBinderHolder, position: Int) {
+    override fun onBindViewHolder(holder: ComplexBinderHolder<ViewDataBinding>, position: Int) {
         val item = getItem(position)
-        if (extraPositionsLayoutMap[position] != null) {
-            extraPositionsLayoutMap[position]!!.onBind.invoke(holder.binding, position)
-        }
-        item?.apply {
-            val extraCount = extraPositionList.count { extraPosition -> position > extraPosition }
-            onBind(holder.binding as B, item, position - extraCount)
-        }
+        val layoutId = layoutConfig.invoke(position, item)
+        layoutToBindMap[layoutId]?.invoke(holder.binding, item, position)
     }
 
     fun setList(list: List<Value>) {
         val actualList = ArrayList(list)
-        val submittedList = ArrayList(list)
-        extraPositionList.forEach {
-            submittedList.add(it, null)
+        submitList(actualList)
+        dataList = actualList
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filteredList = ArrayList<Value>()
+                filteredList.addAll(dataList.filter {
+                    constraint.isNullOrEmpty() ||
+                            filterCondition.invoke(it, constraint.toString())
+                })
+                val results = FilterResults()
+                results.values = filteredList
+                return results
+            }
+
+            override fun publishResults(p0: CharSequence?, filterResults: FilterResults?) {
+                submitList(filterResults?.values as List<Value>)
+            }
         }
-        this.dataList = actualList
-        this.submittedList = submittedList
-
-        mainLaunch {
-            submitList(submittedList)
-        }
-    }
-
-    fun filter(constraint: String?) {
-        bgBlock {
-            val list = dataList.filter { filterCondition.invoke(it, constraint) }
-            mainLaunch { submitList(list) }
-        }
-    }
-
-    fun setFilterCondition(filterCondition: (item: Value, text: String?) -> Boolean) {
-        this.filterCondition = filterCondition
-    }
-
-    fun <DB : ViewDataBinding> addExtraLayout(
-        position: Int,
-        @LayoutRes layoutId: Int,
-        onBind: (binding: DB, position: Int) -> Unit
-    ) {
-        extraPositionList.add(position)
-        extraPositionsLayoutMap[position] = ExtraLayouts(
-            position, layoutId,
-            onBind as (ViewDataBinding, Int) -> Unit
-        )
-    }
-
-    private data class ExtraLayouts(
-        val position: Int,
-        val layoutId: Int,
-        val onBind: (binding: ViewDataBinding, position: Int) -> Unit
-    )
-
-    enum class Equality(val value: Boolean) {
-        ITEMS_ARE_SAME(true), ITEMS_ARE_DIFFERENT(false), CONTENTS_ARE_SAME(true), CONTENTS_ARE_DIFFERENT(
-            false
-        )
     }
 }
 
-/**
- * Type is not used in this class as it can be used for multiple layouts and not just single layout
- */
-class ComplexBinderHolder(val binding: ViewDataBinding) :
+class ComplexBinderHolder<B : ViewDataBinding>(val binding: B) :
     RecyclerView.ViewHolder(binding.root)
