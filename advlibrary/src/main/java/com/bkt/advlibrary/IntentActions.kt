6fb4e0819479b98c9169875b7069342baac68aab
sourav <souravsouravnag@gmail.com>
2024-09-01 16:59:05 +0530

    Added: open and share intents

--- advlibrary/src/main/java/com/bkt/advlibrary/IntentActions.kt
+++ advlibrary/src/main/java/com/bkt/advlibrary/IntentActions.kt
@@ -3,6 +3,10 @@ package com.bkt.advlibrary
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.webkit.MimeTypeMap
+import androidx.core.app.ShareCompat
+import androidx.core.content.FileProvider
+import java.io.File
 
 object IntentActions {
     /**
@@ -55,6 +59,76 @@ object IntentActions {
         }
     }
 
+    fun getMimeType(uri: Uri): String? {
+        val m = MimeTypeMap.getSingleton()
+        val extension = MimeTypeMap.getFileExtensionFromUrl(uri.toString())
+        return m.getMimeTypeFromExtension(extension)
+    }
+
+    /**
+     * Simple method to open any file
+     */
+    fun openFile(activity: CommonActivity, uri: Uri) {
+        val mimeType = getMimeType(uri)
+        val intent = Intent()
+        intent.action = Intent.ACTION_VIEW
+        intent.setDataAndType(uri, mimeType)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        activity.startActivity(intent)
+    }
+
+    /**
+     * Overloaded function with [File] based parameter
+     */
+    fun openFile(activity: CommonActivity, file: File, authority: String) {
+        openFile(activity, FileProvider.getUriForFile(activity, authority, file))
+    }
+
+    fun share(
+        activity: CommonActivity,
+        authority: String,
+        title: String? = null,
+        mimeTypeText: String? = null,
+        subject: String? = null,
+        body: String? = null,
+        htmlBody: String? = null,
+        vararg attachments: File
+    ): ShareCompat.IntentBuilder {
+        return share(
+            activity,
+            title = title,
+            mimeTypeText = mimeTypeText,
+            subject = subject,
+            body = body,
+            htmlBody = htmlBody,
+            *attachments.map { FileProvider.getUriForFile(activity, authority, it) }.toTypedArray()
+        )
+    }
+
+    fun share(
+        activity: CommonActivity,
+        title: String? = null,
+        mimeTypeText: String? = null,
+        subject: String? = null,
+        body: String? = null,
+        htmlBody: String? = null,
+        vararg attachmentUris: Uri
+    ): ShareCompat.IntentBuilder {
+        val b = ShareCompat.IntentBuilder(activity)
+        // get the mime of first file
+        var mime = mimeTypeText
+        attachmentUris.forEach { uri ->
+            if (mime == null)
+                mime = getMimeType(uri)
+            b.addStream(uri)
+        }
+        title?.let { b.setChooserTitle(it) }
+        subject?.let { b.setSubject(it) }
+        body?.let { b.setText(it) }
+        htmlBody?.let { b.setHtmlText(it) }
+        return b
+    }
+
     enum class MimeType(val mimeTypeText: String) {
         ALL("*/*"), IMAGE("image/*"), TEXT_PLAIN("text/plain"),
         DOCUMENT_ALL("application/*"), DOCUMENT_PDF("application/pdf")
@@ -63,6 +137,44 @@ object IntentActions {
 
         fun getSpecificType(type: String): String {
             return this.mimeTypeText.replace("/*", "/$type")
+        }
+    }
+}
+
+class SharingOptions private constructor() {
+    internal var emailsTo = ArrayList<String>()
+    internal var subject = ""
+    internal var title = "Share"
+    internal var body = ""
+    internal var applicationPackage = ""
+
+    fun addToEmail(email: String) {
+        this.emailsTo.add(email)
+    }
+
+    fun addToEmails(emails: List<String>) {
+        this.emailsTo.addAll(emails)
+    }
+
+    fun addSubject(subject: String) {
+        this.subject = subject
+    }
+
+    fun addBody(body: String) {
+        this.body = body
+    }
+
+    fun addChooserTitle(title: String) {
+        this.title = title
+    }
+
+    fun specifyApplicationPackage(applicationPackage: String) {
+        this.applicationPackage = applicationPackage
+    }
+
+    companion object {
+        fun get(): SharingOptions {
+            return SharingOptions()
         }
     }
 }
