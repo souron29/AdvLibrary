45907e177a67cd69cdb7405cba6c81003f072868
sourav <souravsouravnag@gmail.com>
2024-10-05 12:44:32 +0530

    Changed: Fixed multi window mode by removing traces of Fragments inside PagerAdapter

--- advlibrary/src/main/java/com/bkt/advlibrary/PagerAdapter.kt
+++ advlibrary/src/main/java/com/bkt/advlibrary/PagerAdapter.kt
@@ -1,64 +1,30 @@
 package com.bkt.advlibrary
 
-import android.util.SparseArray
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
+/**
+ * This implementation fixes multi window mode
+ * Fragment instances are created and managed by FragmentStateAdapter. So we cannot keep track of the fragments directly
+ * However we can use [androidx.fragment.app.FragmentManager]'s fragments and backStackEntryCount
+ */
 class PagerAdapter(
-    private val parent: CommonFragment
+    parent: CommonFragment,
+    var properties: PagerProperties
 ) : FragmentStateAdapter(parent) {
-
-    private val fragments = SparseArray<CommonFragment>()
-    private val fragmentList = ArrayList<CommonFragment>()
-
-    fun getExistingFragments(): ArrayList<CommonFragment> {
-        return ArrayList(fragments.getValueList())
-    }
+    val fragmentManager = parent.childFragmentManager
+    fun getFragments(): MutableList<Fragment> = fragmentManager.fragments
+    fun getFragment(position: Int): Fragment? = getFragments().getOrNull(position)
 
     override fun getItemCount(): Int {
-        return fragments.size()
+        return properties.pageCount
     }
 
     override fun createFragment(position: Int): Fragment {
-        return fragments[position]
-    }
-
-    fun addFragment(fragment: CommonFragment): Int {
-        fragmentList.add(fragment)
-        fragments[fragmentList.indexOf(fragment)] = fragment
-        return fragmentList.size - 1
-    }
-
-    fun removeFragment(fragment: CommonFragment) {
-        val index = fragmentList.indexOf(fragment)
-        fragmentList.removeAt(index)
-        fragments.remove(index)
-    }
-
-    fun clearAllFragments() {
-        fragmentList.clear()
-        fragments.clear()
-    }
-
-    fun addAllFragments(listOfFragments: List<CommonFragment>) {
-        listOfFragments.forEach {
-            addFragment(it)
-        }
-    }
-
-    fun removeAt(index: Int) {
-        fragmentList.removeAt(index)
-        fragments.remove(index)
-    }
-
-    fun replaceFragment(fragment: CommonFragment, index: Int) {
-        logger("Fragment ${fragments[index]}")
-        fragments[index] = fragment
-        fragmentList[index] = fragment
-        logger("Fragment ${fragments[index]}")
+        return properties.fragCreator.invoke(position)
     }
 
     fun setTab(tabLayout: TabLayout, pager: ViewPager2, block: (Int) -> String) {
@@ -66,8 +32,6 @@ class PagerAdapter(
             tab.text = block.invoke(position)
         }.attach()
     }
-
-    fun getFragment(pos: Int): CommonFragment {
-        return fragments[pos]
-    }
 }
+
+data class PagerProperties(val pageCount: Int, val fragCreator: (Int) -> CommonFragment)
