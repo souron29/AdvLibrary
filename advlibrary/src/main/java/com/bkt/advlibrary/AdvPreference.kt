f3c7a2ad1da2aa1dd98866ea38d11f18cb54e35e
souravnag <souravsouravnag@gmail.com>
2026-03-07 20:12:20 +0530

    Changed: Cleaned the class and functionalities

---
+++ advlibrary/src/main/java/com/bkt/advlibrary/AdvPreference.kt
@@ -0,0 +1,138 @@
+package com.bkt.advlibrary
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class AdvPreference<T>(
+    context: Context,
+    private val key: String,
+    private val defaultValue: T
+) : ReadWriteProperty<Any?, T> {
+    private val prefs by lazy { PreferenceManager.getDefaultSharedPreferences(context) }
+
+    val flow: Flow<T> by lazy {
+        callbackFlow {
+            val listener =
+                SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, changedKey ->
+                    if (key == changedKey) {
+                        trySend(get())
+                    }
+                }
+
+            prefs.registerOnSharedPreferenceChangeListener(listener)
+
+            // Ensure the flow starts with the current value immediately
+            trySend(get())
+
+            // Unregister the listener when the flow is closed/cancelled
+            awaitClose {
+                prefs.unregisterOnSharedPreferenceChangeListener(listener)
+            }
+        }
+    }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return get()
+    }
+
+    fun get() = findPreference(key, defaultValue)
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        set(value)
+    }
+
+    fun set(value: T) = putPreference(key, value)
+
+    fun remove() = prefs.edit { remove(key) }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun findPreference(name: String, default: T): T = with(prefs) {
+        val res: Any? = when (default) {
+            is Long -> getLong(name, default)
+            is String -> getString(name, default)
+            is Int -> getInt(name, default)
+            is Boolean -> getBoolean(name, default)
+            is Float -> getFloat(name, default)
+            else -> throw IllegalArgumentException("This type cannot be saved into Preferences")
+        }
+        res as T
+    }
+
+    private fun putPreference(name: String, value: T) = with(prefs.edit()) {
+        when (value) {
+            is Long -> putLong(name, value)
+            is String -> putString(name, value)
+            is Int -> putInt(name, value)
+            is Boolean -> putBoolean(name, value)
+            is Float -> putFloat(name, value)
+            else -> throw IllegalArgumentException("This type cannot be saved into Preferences")
+        }.apply()
+    }
+
+    companion object {
+        fun clearAllPreferences(context: Context): SharedPreferences.Editor? {
+            return context.getPreferences().edit().clear()
+        }
+
+        fun getStringPreferences(context: Context, key: String, default: String = ""): String {
+            return context.getPreferences().getString(key, default) ?: default
+        }
+
+        fun getIntPreferences(context: Context, key: String, default: Int = 0): Int {
+            return context.getPreferences().getInt(key, default)
+        }
+
+        fun getBoolPreferences(context: Context, key: String, default: Boolean = false): Boolean {
+            return context.getPreferences().getBoolean(key, default)
+        }
+
+        fun writeStringToPreferences(
+            context: Context,
+            key: String,
+            value: String,
+            ifEmpty: Boolean = false
+        ) {
+            if (!ifEmpty) {
+                context.getPreferences().edit { putString(key, value) }
+            } else if (getStringPreferences(context, key).isEmpty()) {
+                context.getPreferences().edit { putString(key, value) }
+            }
+        }
+
+        fun writeBoolToPreferences(
+            context: Context,
+            key: String,
+            value: Boolean,
+            ifEmpty: Boolean
+        ) {
+            if (!ifEmpty) {
+                context.getPreferences().edit { putBoolean(key, value) }
+            } else if (getStringPreferences(context, key).isEmpty()) {
+                context.getPreferences().edit { putBoolean(key, value) }
+            }
+        }
+
+        fun writeIntToPreferences(
+            context: Context,
+            key: String,
+            value: Int,
+            ifEmpty: Boolean
+        ) {
+            if (!ifEmpty) {
+                context.getPreferences().edit { putInt(key, value) }
+            } else if (getStringPreferences(context, key).isEmpty()) {
+                context.getPreferences().edit { putInt(key, value) }
+            }
+        }
+    }
+}
+
+fun Context.getPreferences(): SharedPreferences =
+    PreferenceManager.getDefaultSharedPreferences(this)
